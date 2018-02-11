9705369ec69bb037e9b3fab8963c6611ffa8c116
Sean Bostic <sean.bostic@gmail.com>
2018-02-10 20:06:36 -0500

    Adding print message.
    Add a counter message to print out how many files have been deleted.

--- cleanup.py
+++ cleanup.py
@@ -4,3 +4,9 @@ import os
 for file in os.scandir(r"C:\Users\Sean\Downloads"):
     if file.name.endswith(".exe"):
         os.unlink(file.path)
+print(".exe files have been deleted")
+
+for file in os.scandir(r"C:\Users\Sean\Desktop"):
+    if file.name.endswith(".zip"):
+        os.unlink(file.path)
+print(".zip files have been deleted")
